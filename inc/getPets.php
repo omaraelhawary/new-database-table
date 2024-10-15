6a6d6b3d3bebc4db5140f70a901b40ddecb6a71c
Omar ElHawary <omaraelhawary@gmail.com>
2024-10-15 22:52:57 +0300

    Create Dynamic query

--- inc/getPets.php
+++ inc/getPets.php
@@ -4,7 +4,65 @@ class getPets {
     public function __construct() {
         global $wpdb;
         $tablename = $wpdb->prefix . 'pets';
-        $ourQuery = $wpdb->prepare("SELECT * from $tablename LIMIT 100");
-        $this -> pets = $wpdb->get_results($ourQuery);
+
+        $this->args = $this->getArgs();
+
+        $query = "SELECT * FROM $tablename ";
+        $query .= $this->createWhereText();
+        $query .= " LIMIT 100";
+
+        echo $query;
+
+        $this -> pets = $wpdb->get_results($wpdb -> prepare( $query, $this->args));
+    }
+
+    function getArgs() {
+        $temp = [];
+     
+        if (isset($_GET['favcolor'])) $temp['favcolor'] = sanitize_text_field($_GET['favcolor']);
+        if (isset($_GET['species'])) $temp['species'] = sanitize_text_field($_GET['species']);
+        if (isset($_GET['minyear'])) $temp['minyear'] = sanitize_text_field($_GET['minyear']);
+        if (isset($_GET['maxyear'])) $temp['maxyear'] = sanitize_text_field($_GET['maxyear']);
+        if (isset($_GET['minweight'])) $temp['minweight'] = sanitize_text_field($_GET['minweight']);
+        if (isset($_GET['maxweight'])) $temp['maxweight'] = sanitize_text_field($_GET['maxweight']);
+        if (isset($_GET['favhobby'])) $temp['favhobby'] = sanitize_text_field($_GET['favhobby']);
+        if (isset($_GET['favfood'])) $temp['favfood'] = sanitize_text_field($_GET['favfood']);
+     
+        return $temp;
+     
+      }
+
+    function createWhereText() {
+        $whereQuery = "";
+
+        if(count($this->args) > 0) {
+            $whereQuery = "WHERE ";
+        }
+
+        $currentPostion = 0;
+
+        foreach($this -> args as $index => $item){
+            $whereQuery .= $this->specificQuery($index);
+            if ($currentPostion != count($this->args)-1) {
+               $whereQuery .= " AND "; 
+            }
+            $currentPostion++;
+        }
+        return $whereQuery;
+    }
+
+    function specificQuery($index) {
+        switch ($index) {
+            case "minweight":
+                return "petweight >= %d";
+            case "maxweight":
+                return "petweight <= %d";
+            case "minyear":
+                return "birthyear >= %d";
+            case "maxyear":
+                return "birthyear <= %d";
+            default:
+                return $index . " = %s";
+        }
     }
 }
